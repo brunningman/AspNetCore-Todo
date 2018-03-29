60649e0b8e44eb5bc66b47bd01d9fefa53fda567
BrunningMan <abrunner221@gmail.com>
2018-03-29 08:33:35 -0700

    initial commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        // Actions go here
+        public IActionResult Index()
+        {
+            // Get to-do items from database
+
+            // Put items into a model
+
+            // Render view using the model
+        }
+    }
+}
